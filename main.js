572bff3b0377b55e596333d11747dd24bda4077b
Tatiana Osipova <otv120780@gmail.com>
2023-03-28 12:29:38 -0400

    feat: add file tile.js

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import { Grid } from "./grid.js";
 const gameBoard = document.getElementById("game-board");
 
 const grid = new Grid(gameBoard);
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
 
