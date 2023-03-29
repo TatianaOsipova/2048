2aa6e1d37d493fca59f4c347dcc33388b74c3963
Tatiana Osipova <otv120780@gmail.com>
2023-03-29 15:50:37 -0400

    feat: add second tile

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ const gameBoard = document.getElementById("game-board");
 
 const grid = new Grid(gameBoard);
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
 
