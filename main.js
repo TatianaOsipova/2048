4c0cc2eb793fb27b011683fd7242496b71118f4f
Tatiana Osipova <otv120780@gmail.com>
2023-03-29 15:57:01 -0400

    feat: add arrows

--- main.js
+++ main.js
@@ -6,4 +6,30 @@ const gameBoard = document.getElementById("game-board");
 const grid = new Grid(gameBoard);
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+setupInputOnce();
+
+function setupInputOnce() {
+    window.addEventListener("keydown", handleInput, {once: true});
+}
+
+function handleInput(event) {
+    switch (event.key) {
+        case "ArrowUp":
+        break;
+
+        case "ArrowDown":
+        break;
+
+        case "ArrowLeft":
+        break;
+
+        case "ArrowRight":
+        break;
+
+        default:
+        break;
+    }
+
+    setupInputOnce();
+}
 
